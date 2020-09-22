c0fe0ac9a60fe96e49edaad56927868010d2666d
luckpp <marius.andritoi@yahoo.com>
2020-09-22 00:05:16 -0700

    added error handling middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,26 +1,27 @@
-import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
 router.post(
-  "/api/users/signup",
+  '/api/users/signup',
   [
-    body("email").isEmail().withMessage("Email must be valid"),
-    body("password")
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage("Password must be between 4 and 20 characters"),
+      .withMessage('Password must be between 4 and 20 characters'),
   ],
   (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new Error('Invalid email or password');
     }
 
     const { email, password } = req.body;
 
-    console.log("Creating a user...");
+    console.log('Creating a user...');
+    throw new Error('Error connecting to database');
 
     res.send({});
   }
