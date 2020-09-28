64603f27b9fd4cd648c866d0066eb5ee4e6ceddc
luckpp <marius.andritoi@yahoo.com>
2020-09-28 01:45:43 -0700

    implemented /auth/currentuser route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hello from Router!');
+router.get('/api/users/currentuser', (req: Request, res: Response) => {
+  // the check below is equivalent to (!req.session || !req.session.jwt)
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
