4699fc14890d5660e47de63b55a0f69eec27c875
luckpp <marius.andritoi@yahoo.com>
2020-09-29 01:42:33 -0700

    extracted signin functionality and added a new current-user test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,16 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+
 import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 
@@ -27,3 +37,24 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+// we will create a global helper function fo the ease of use
+// one could also choose to create it in separate file, export it and import it whenever needed
+// since we are defining the function here it will be available in our app only in the test environment
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
