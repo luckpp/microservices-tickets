38e6c525ea3a5807f821f034784d71d44cd035b1
luckpp <marius.andritoi@yahoo.com>
2020-10-06 23:23:41 -0700

    tested for ticket update rights

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import mongoose from 'mongoose';
 import { app } from '../../app';
+import { response } from 'express';
 
 it('returns 404 if the provided id does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -25,7 +26,26 @@ it('returns 401 if the use is not authenticated', async () => {
     .expect(401);
 });
 
-it('returns 401 if the use does not own the ticket', async () => {});
+it('returns 401 if the use does not own the ticket', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'Title 1',
+      price: 20,
+    })
+    .expect(201);
+  const ticket = response.body;
+
+  await request(app)
+    .put(`/api/tickets/${ticket.id}`)
+    .set('Cookie', global.signin())
+    .send({
+      title: 'Title 2',
+      price: 20,
+    })
+    .expect(401);
+});
 
 it('returns 401 if the use provides an invalid title or price', async () => {});
 
