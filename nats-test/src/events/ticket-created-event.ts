04cd925897ddbdb493bc42f219c3047aaf34b702
luckpp <marius.andritoi@yahoo.com>
2020-10-12 09:02:05 -0700

    creating type safe NATS listeners and events

---
+++ nats-test/src/events/ticket-created-event.ts
@@ -0,0 +1,11 @@
+import { title } from 'process';
+import { Subjects } from './subjects';
+
+export interface TicketCreatedEvent {
+  subject: Subjects.TicketCreated;
+  data: {
+    id: string;
+    title: string;
+    price: number;
+  }
+}
--- nats-test/src/events/ticket-createde-listener.ts
+++ nats-test/src/events/ticket-createde-listener.ts
@@ -1,11 +1,13 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
+import { Subjects } from './subjects';
+import { TicketCreatedEvent } from './ticket-created-event';
 
-export class TicketCreatedListener extends Listener {
-  subject: string = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject: Subjects.TicketCreated =  Subjects.TicketCreated;
   queueGroupName: string = 'payments-service';
 
-  onMessage(data: any, msg: Message): void {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
     console.log('Event data!', data);
 
     msg.ack();
