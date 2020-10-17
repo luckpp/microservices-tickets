1fad23e51c82b60e361120e6426d0ee0836a84a7
luckpp <marius.andritoi@yahoo.com>
2020-10-17 00:49:26 -0700

    testing the show order route handler

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('can only be accessed with valid orderId', async () => {
   const orderId = 'wrong_mongo_id';
@@ -8,4 +9,61 @@ it('can only be accessed with valid orderId', async () => {
     .set('Cookie', global.signin())
     .send()
     .expect(400);
+});
+
+it('fetches the order', async () => {
+  const user = global.signin();
+
+  // Create the ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 10,
+  });
+  await ticket.save();
+
+  // Make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+
+  // Make a request to fetch the order
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+
+  expect(fetchedOrder.id).toEqual(order.id);
+  expect(fetchedOrder.ticket.id).toEqual(ticket.id);
+});
+
+it('returns an error if one user tries to fetch another users error', async () => {
+  // Create the ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 10,
+  });
+  await ticket.save();
+
+  // Make a request to build an order with this ticket
+  const userOne = global.signin();
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', userOne)
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+
+  // Make a request to fetch the order
+  const userTwo = global.signin();
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', userTwo)
+    .send()
+    .expect(401);
 });
