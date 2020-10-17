d9fad3ab812fdee287b0990f4ac8719bb2a32a3e
luckpp <marius.andritoi@yahoo.com>
2020-10-17 08:54:37 -0700

    updated the common library with order:created and order:cancelled events

---
+++ common/src/events/order-created-event.ts
@@ -0,0 +1,18 @@
+import { Subjects } from './subjects';
+import { OrderStatus } from './types/order-status';
+
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    userId: string;
+    status: OrderStatus;
+    // inside the order we use Date type for expiresAt, but since we want to convert that Date to a JSON
+    // or a string manually in order to control how the time-zone gets set so we will use a string
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
+  };
+}
