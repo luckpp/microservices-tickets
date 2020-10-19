82f6d59980b8dd1ba3c97a2e74ba8ec377767222
luckpp <marius.andritoi@yahoo.com>
2020-10-19 02:45:23 -0700

    started to listen for ticket events inside orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import { natsWrapper } from './nats-wrapper';
 
 import { app } from './app';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -22,8 +24,8 @@ const start = async () => {
 
   try {
     await natsWrapper.connect(
-      process.env.NATS_CLUSTER_ID, 
-      process.env.NATS_CLIENT_ID, 
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
       process.env.NATS_URL
     );
     natsWrapper.client.on('close', () => {
@@ -32,7 +34,10 @@ const start = async () => {
     });
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
-    
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
