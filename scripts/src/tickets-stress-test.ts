5c38b2c78a4a8d0736724e022e166aa89dcd18d2
luckpp <marius.andritoi@yahoo.com>
2020-10-19 05:05:55 -0700

    started to add test scripts

---
+++ scripts/src/tickets-stress-test.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+const cookie =
+  'express:sess=eyJqd3QiOiJleUpoYkdjaU9pSklVekkxTmlJc0luUjVjQ0k2SWtwWFZDSjkuZXlKcFpDSTZJalZtT0dRM1l6ZzRZVFpoTVRObE1EQXlOVGMwT0dSbVlpSXNJbVZ0WVdsc0lqb2lkR1Z6ZEVCMFpYTjBMbU52YlNJc0ltbGhkQ0k2TVRZd016RXdOemszTm4wLkdXdkF5UEpJM0V5azNVRVR5ekszZTdUcGhGS2JnbUN4MzU0RURGSWlpbnMifQ==; Path=/; Domain=tickets.dev; Secure; HttpOnly;';
+
+const doRequest = async () => {
+  const { data } = await axios.post(
+    'https://tickets.dev/api/tickets',
+    { title: 'ticket', price: 5 },
+    {
+      headers: { cookie },
+    }
+  );
+
+  await axios.put(
+    `https://tickets.dev/api/tickets/${data.id}`,
+    { title: 'ticket', price: 10 },
+    {
+      headers: { cookie },
+    }
+  );
+
+  await axios.put(
+    `https://tickets.dev/api/tickets/${data.id}`,
+    { title: 'ticket', price: 15 },
+    {
+      headers: { cookie },
+    }
+  );
+};
+
+for (let count = 0; count < 200; count++) {
+  doRequest();
+}
