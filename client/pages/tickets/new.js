f209bdc1a5385f5c3dd5e3b3edff27f9d92ee083
luckpp <marius.andritoi@yahoo.com>
2020-10-29 13:09:17 -0700

    call new ticket api from client

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,23 +1,38 @@
 import { useState } from 'react';
+import useRequest from '../../hooks/use-request';
 
 const NewTicket = () => {
   const [title, setTitle] = useState('');
   const [price, setPrice] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/tickets',
+    method: 'post',
+    body: {
+      title,
+      price,
+    },
+    onSuccess: (ticket) => {
+      console.log(ticket);
+    },
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    doRequest();
+  };
 
   const onBlur = () => {
     const value = parseFloat(price);
-
     if (isNaN(value)) {
       return;
     }
-
     setPrice(value.toFixed(2));
   };
 
   return (
     <div>
       <h1>Create a Ticket</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label>Title</label>
           <input
@@ -35,6 +50,7 @@ const NewTicket = () => {
             className="form-control"
           ></input>
         </div>
+        {errors}
         <button className="btn btn-primary">Submit</button>
       </form>
     </div>
