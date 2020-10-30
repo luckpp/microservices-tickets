bd5fa8682c3c2ed5514cbc45b2761e4c7efec9dd
luckpp <marius.andritoi@yahoo.com>
2020-10-30 00:32:33 -0700

    get the ticket list in the landing page of the client

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
 const NewTicket = () => {
@@ -13,6 +14,7 @@ const NewTicket = () => {
     },
     onSuccess: (ticket) => {
       console.log(ticket);
+      Router.push('/');
     },
   });
 
